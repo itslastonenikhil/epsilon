bd9fd1e28c46c8d8cae376e24739ceb185fa6c5d
itslastonenikhil <nikhilmaurya682000@gmail.com>
2021-11-29 15:29:04 +0530

    Added GPS event counter

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function geoFindMe() {
     status.textContent = 'Geolocation is not supported by your browser';
   } else {
     status.textContent = 'Locating…';
-    navigator.geolocation.getCurrentPosition(success, error, options);
+    navigator.geolocation.watchPosition(success, error, options);
   }
 
 }
